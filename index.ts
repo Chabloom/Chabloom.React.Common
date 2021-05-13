d7541f91ef56bcdf6ba7d0d08a61256aa2d957fd
Matthew Casey <mdcasey@chabloom.com>
2021-05-13 00:14:08 -0400

    Add window configuration typing

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+export interface Configuration {
+  REACT_APP_ACCOUNTS_FRONTEND_ADDRESS: string;
+  REACT_APP_BILLING_FRONTEND_ADDRESS: string;
+  REACT_APP_ECOMMERCE_FRONTEND_ADDRESS: string;
+  REACT_APP_TRANSACTIONS_FRONTEND_ADDRESS: string;
+  REACT_APP_ACCOUNTS_BACKEND_ADDRESS: string;
+  REACT_APP_BILLING_BACKEND_ADDRESS: string;
+  REACT_APP_ECOMMERCE_BACKEND_ADDRESS: string;
+  REACT_APP_TRANSACTIONS_BACKEND_ADDRESS: string;
+}
+
+declare global {
+  interface Window {
+    __env__: Configuration;
+  }
+}
+
+window.__env__ = window.__env__ || {};
+
 export * from "./api";
 export * from "./components";
 export * from "./images";
